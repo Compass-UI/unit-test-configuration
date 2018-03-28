0f8e2d5aaca4cad6d7fb39e65b3b060efec1229b
David Shams <david.shams@united.com>
2018-03-28 15:56:59 -0500

    testing AMD module: BUG define is not defined problem

---
+++ src/js/aeroSpec.js
@@ -0,0 +1,25 @@
+
+describe('IN AERO.JS:=========================  ', function(){
+    describe('Testing constants', function(){
+        it('Should match the values', function(){
+            /** Mock ( i.e. exclude jQuery which aero depends on ) */
+            define('jquery', [], function(){
+                return {
+                    jquery: function(){
+                        /** Write like your own sort of jQuery */
+                    }
+                }
+            })
+            require(['aero', 'jquery'], function(aero, $){
+
+                expect(aero.const.version).toEqual('3.0')
+                    
+                // spyOn(aero, "doLogin");
+                // ccAero.doLogin();
+                // expect(ccAero.doLogin).toHaveBeenCalled();
+                
+            })
+            
+        })
+    })
+})
